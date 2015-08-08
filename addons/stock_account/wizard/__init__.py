946c90254b430cfbedb76b594b83d0c88b4d71ba
Fabien Pinckaers <fp@openerp.com>
2015-08-08 03:38:59 +0200

    [IMP] removing unused code

--- addons/stock_account/wizard/__init__.py
+++ addons/stock_account/wizard/__init__.py
@@ -2,6 +2,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import stock_change_standard_price
-import stock_invoice_onshipping
 import stock_valuation_history
 import stock_return_picking
--- addons/stock_dropshipping/stock_dropshipping.py
+++ addons/stock_dropshipping/stock_dropshipping.py
@@ -3,26 +3,6 @@
 from openerp import models, api, _
 from openerp.exceptions import UserError
 
-
-class sale_order_line(models.Model):
-    _inherit = 'sale.order.line'
-
-    @api.multi
-    def _check_routing(self, product, warehouse):
-        """ skip stock verification if the route goes from vendor to customer
-            As the product never goes in stock, no need to verify it's availibility
-        """
-        res = super(sale_order_line, self)._check_routing(product, warehouse)
-        if not res:
-            for line in self:
-                for pull_rule in line.route_id.pull_ids:
-                    if (pull_rule.picking_type_id.default_location_src_id.usage == 'supplier' and
-                            pull_rule.picking_type_id.default_location_dest_id.usage == 'customer'):
-                        res = True
-                        break
-        return res
-
-
 class purchase_order(models.Model):
     _inherit = 'purchase.order'
 
